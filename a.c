d7f8b8bf51ce5d4b9cc3170ecb9d448f3b3e0dd1
hiyellowc <hiyellow@gmail.com>
2015-10-29 23:54:40 +0800

    2015-10-30+

--- a.c
+++ a.c
@@ -3,17 +3,6 @@
 #include <time.h>
 
 int main() {
-	int t = 14;
-	while(t--) {
-		srand(time(NULL));
-		printf("%d\n", 499999);
-		for(int i = 1; i < 250000; i++) {
-			int num = rand() % 1000000;
-			int flag = rand() % 2;
-			if(flag) num *= -1;
-			printf("%d %d ", num, num);
-		}
-		printf("1000000\n");
-	}
+	printf("hello");
 	return 0;
 }
