7d3977073ee2a9b1f5294cc272000b073d8b65f0
hiyellowc <hiyellowc@gmail.com>
2015-11-08 21:28:59 +0800

    2015-11-08

--- 119+.cpp
+++ 119+.cpp
@@ -1,10 +1,6 @@
-#include <stdio.h>
-#include <math.h>
-#define Max 100001
-
-int num = (int)(log(Max) / log(2));
-int result[Max][log()];
+#include <bits/stdc++.h>
+using namespace std;
 
 int main() {
 	return 0;
-}
+} 
